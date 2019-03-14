56df05d262c356eed11834dfb0ba1335bcf175ab
Donelys Familia <donelys.familia@niocoders.com>
2019-03-14 16:49:27 -0400

    adding Vasya challenge

--- src/challeges_from_codewars/Line.java
+++ src/challeges_from_codewars/Line.java
@@ -2,7 +2,18 @@ package challeges_from_codewars;
 
 	public class Line {
 		
-		
+		/**
+		 * 
+		 * @param args
+		 * 
+		 * The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
+
+			Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+			Can Vasya sell a ticket to each person and give the change if he initially has no money and sells the tickets strictly in the order people follow in the line?
+
+			Return YES, if Vasya can sell a ticket to each person and give the change with the bills he has at hand at that moment. Otherwise return NO.
+		 */
 		public static void main(String[] args ){
 			System.out.println(Line.Tickets(new int[] {25, 25, 50})); // => YES 
 			
