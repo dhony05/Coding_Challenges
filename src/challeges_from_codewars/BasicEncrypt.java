c42487ebe21e7ed9221774fa50c58d1dc32d351b
Donelys <donelysprivate@gmail.com>
2019-08-13 19:45:22 -0400

    another update with hashmap

---
+++ src/challeges_from_codewars/BasicEncrypt.java
@@ -0,0 +1,14 @@
+package challeges_from_codewars;
+
+public class BasicEncrypt {
+
+    public String encrypt(String text, int rule) {
+
+        return text;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
