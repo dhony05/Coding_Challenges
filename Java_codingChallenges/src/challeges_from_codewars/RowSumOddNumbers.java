fd92d87ec903c187693d5a1fde8ada0088b3a973
Donelys Familia <donelysprivate@gmail.com>
2019-10-04 08:48:37 -0400

    adding challenge description

--- Java_codingChallenges/src/challeges_from_codewars/RowSumOddNumbers.java
+++ Java_codingChallenges/src/challeges_from_codewars/RowSumOddNumbers.java
@@ -1,6 +1,18 @@
 package challeges_from_codewars;
 
 public class RowSumOddNumbers {
+	
+	//Given the triangle of consecutive odd numbers:
+	//  1
+   // 3     5
+// 7     9    11
+//13    15    17    19
+//21    23    25    27    29
+//...
+	
+	//Calculate the row sums of this triangle from the row index (starting at index 
+	//rowSumOddNumbers(1); // 1
+	//rowSumOddNumbers(2); // 3 + 5 = 8
 
 	public static void main(String[] args) {
 		
