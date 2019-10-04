db5781ffea55c6b87f4e1926e9c6428e1f458522
Donelys Familia <donelysprivate@gmail.com>
2019-10-03 21:40:47 -0400

    adding different way to do this challenge

---
+++ Java_codingChallenges/src/challeges_from_codewars/RowSumOddNumbers.java
@@ -0,0 +1,48 @@
+package challeges_from_codewars;
+
+public class RowSumOddNumbers {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println(rowSumOddNumbers(1)); // 1
+		System.out.println(rowSumOddNumbers(2)); // 3 + 5 = 8
+		
+		System.out.println(rowSumOddNumbers2(1));
+		System.out.println(rowSumOddNumbers2(2));
+	}
+	
+	
+	//first way to do it  O(n) time complexity
+	
+	public static int rowSumOddNumbers(int n) {
+		// formula n*(n-1) + 1
+		int counter = 0;
+		int result = 0;
+		for (int i = (n*(n-1))+1; counter < n; i++) {
+			if(i%2 == 1) {
+				counter ++;
+				result += i;
+			}
+		}
+		return result;
+	       
+    }
+	
+	//contant time complexity 
+	
+	public static int rowSumOddNumbers2(int n) {
+	
+	return n* n* n;
+		
+	}
+	
+	
+	public static int rowSumOddNumbers3(int n) {
+		
+		return (int) Math.pow(n,3);
+			
+		}
+
+
+}
