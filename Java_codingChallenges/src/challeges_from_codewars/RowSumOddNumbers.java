71a7baba4bbbcb5996476c21a2717b720bb082fe
Donelys Familia <donelysprivate@gmail.com>
2019-10-04 08:46:19 -0400

    adding a way to show triangle

--- Java_codingChallenges/src/challeges_from_codewars/RowSumOddNumbers.java
+++ Java_codingChallenges/src/challeges_from_codewars/RowSumOddNumbers.java
@@ -7,9 +7,10 @@ public class RowSumOddNumbers {
 
 		System.out.println(rowSumOddNumbers(1)); // 1
 		System.out.println(rowSumOddNumbers(2)); // 3 + 5 = 8
-		
-		System.out.println(rowSumOddNumbers2(1));
-		System.out.println(rowSumOddNumbers2(2));
+		System.out.println(rowSumOddNumbers(3));
+		System.out.println(rowSumOddNumbers(4));
+		//System.out.println(rowSumOddNumbers2(1));
+		//System.out.println(rowSumOddNumbers2(2));
 	}
 	
 	
@@ -17,14 +18,22 @@ public class RowSumOddNumbers {
 	
 	public static int rowSumOddNumbers(int n) {
 		// formula n*(n-1) + 1
+		String format ="";
 		int counter = 0;
 		int result = 0;
 		for (int i = (n*(n-1))+1; counter < n; i++) {
 			if(i%2 == 1) {
+				
 				counter ++;
+				
 				result += i;
+				
+				format = format + " " + Integer.toString(i);
+				
+			
 			}
 		}
+		System.out.println(format + " = sum "+ result);
 		return result;
 	       
     }
