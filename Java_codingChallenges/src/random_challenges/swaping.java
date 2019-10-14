12e66349fa26d783e98f740ad0474ae867f89007
Donelys Familia <donelysprivate@gmail.com>
2019-10-13 23:16:21 -0400

    adding another way to swap

--- Java_codingChallenges/src/random_challenges/swaping.java
+++ Java_codingChallenges/src/random_challenges/swaping.java
@@ -14,6 +14,11 @@ public class swaping {
 		swap_integer(5,7);
 		swap_integer(6,7);
 		swap_integer(7,8);
+		
+		swap_integer2(4,7);
+		swap_integer2(5,7);
+		swap_integer2(6,7);
+		swap_integer2(7,8);
 	}
 	
 	public static void swap_integer(int a, int b){
@@ -25,7 +30,19 @@ public class swaping {
 		
 		a = a - b;  // a is change again a = 7 because  11 - 4 = 7
 		
-		System.out.println("a now is: " + a + " and b is now: "+ b);
+		System.out.println("[ a now is: " + a + " and b is now: "+ b+ " ]");
+		
+	}
+public static void swap_integer2(int a, int b){
+		
+		
+		a = a * b;  
+		
+		b = a / b; 
+		
+		a = a / b;  
+		
+		System.out.println("[ a now is: " + a + " and b is now: "+ b+ " ]");
 		
 	}
 }
