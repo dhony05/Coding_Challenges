2e95fa309fb7bda1e394396486f2b7fc4c77f62f
Donelys Familia <donelysprivate@gmail.com>
2019-10-28 16:46:21 -0400

    adding another way to get a reversed string

--- Java_codingChallenges/src/random_challenges/ReversingSTring.java
+++ Java_codingChallenges/src/random_challenges/ReversingSTring.java
@@ -4,9 +4,10 @@ import java.util.Stack;
 
 public class ReversingSTring {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ReverseString("hello");
 		ReverseString2("hello");
+		ReverseString3("hello");
 	}
 
 	public static void ReverseString(String string) {
@@ -19,13 +20,14 @@ public class ReversingSTring {
 	}
 	
 	
-	public static String ReverseString2(String str) {
+	public static String ReverseString2(String str) throws InterruptedException {
 		Stack<Character> toReverse = new Stack<Character>();
 		
 		
 		for (int i = 0; i < str.length(); i++) {
 			char inChar = str.charAt(i);
 			toReverse.push(inChar);
+			Thread.sleep(1000);
 			System.out.println(toReverse);	
 			
 		}
@@ -35,11 +37,26 @@ public class ReversingSTring {
 		while(!toReverse.isEmpty()) {
 			newStr += toReverse.pop();
 		}
+		Thread.sleep(1000);
 		System.out.println(newStr);
 		return newStr;
 		
 	}
 	
+	
+	public static String ReverseString3(String str) {
+		String reversedString = "";
+		
+		for (int i = str.length()-1; i >= 0; i--) {
+			char inChar = str.charAt(i);
+			reversedString += inChar;
+			System.out.println(inChar);
+		}
+		
+		System.out.println(reversedString);
+		return reversedString;
+		
+	}
 	
 
 }
