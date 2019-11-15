928ea1972a6b6f78acfb1f6e79c066262098cdfe
Donelys Familia <donelysprivate@gmail.com>
2019-11-14 23:32:24 -0500

    finishing  stringtoArray challenge

--- Java_codingChallenges/src/challeges_from_codewars/StringToArray.java
+++ Java_codingChallenges/src/challeges_from_codewars/StringToArray.java
@@ -1,10 +1,18 @@
 package challeges_from_codewars;
 
 public class StringToArray {
-
+/// This Challenge you are asked to convert a string to an array
 	public static void main(String[] args) {
 		
+		toArray("Robin Signh");
 
+	}
+
+	public static String[] toArray(String string) {
+		
+		
+		String [] newA = string.split(" ");
+		return newA;
 	}
 
 }
