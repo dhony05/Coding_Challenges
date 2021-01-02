1200e525174b3d39c297b65c8c0382f3715b761e
Donelys Familia <donelysprivate@gmail.com>
2021-01-02 16:11:50 -0500

    adding get maximum profit challenge

--- Java_codingChallenges/src/random_challenges/maximumProfit.java
+++ Java_codingChallenges/src/random_challenges/maximumProfit.java
@@ -4,6 +4,9 @@ public class maximumProfit {
 
 	
 	/***
+	 * Given a array of numbers representing the stock prices of a company in chronological order,
+	 * write a function that calculates the maximum profit you could have made from buying  and selling that stock once.
+	 * You must buy before you can sell it.
 	 * 
 	 * 
 	 */
@@ -30,8 +33,7 @@ public class maximumProfit {
 			}
 		}
 		
-		
-		
+
 		return tempMaxProf;
 	}
 
