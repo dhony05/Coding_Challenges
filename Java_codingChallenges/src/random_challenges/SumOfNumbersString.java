9c77baa9a456828f3d847f2e6dc365428b6a64a6
Donelys Familia <donelysprivate@gmail.com>
2021-01-03 23:11:49 -0500

    print the sum of all numbers presents in a string

---
+++ Java_codingChallenges/src/random_challenges/SumOfNumbersString.java
@@ -0,0 +1,46 @@
+package random_challenges;
+
+public class SumOfNumbersString {
+
+	public static void main(String[] args) {
+		
+		sumOfNums("eso1y20");//21
+		sumOfNums("ya3mil90m");//93
+		sumOfNums("c0v1d-19");//20
+		sumOfNums("klk9n0pr42i4");//55
+		sumOfNums("num24rrt43");//67
+		sumOfNums("esoy3yrt450");//453
+
+	}
+	
+	
+	
+	public static int sumOfNums(String str) {
+		int currentSum = 0;
+		int maxSum = 0;
+		
+		for(int i = 0; i< str.length(); i++) {
+			if(Character.isDigit(str.charAt(i)) &&(currentSum == 0)) {
+				currentSum = Integer.parseInt(String.valueOf(str.charAt(i)));
+				
+			}
+			else if((Character.isDigit(str.charAt(i))&& (currentSum != 0))){
+					
+				currentSum  = (currentSum * 10) +Integer.parseInt(String.valueOf(str.charAt(i)));    
+				
+			}else {
+				maxSum += currentSum;
+				currentSum = 0;
+				
+			}
+			System.out.println(currentSum +".."+ maxSum);
+			
+		}
+		maxSum += currentSum;
+		
+		System.out.println(maxSum);
+		
+		return maxSum;
+	}
+
+}
