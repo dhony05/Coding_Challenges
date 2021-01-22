e89c0ff4c6e73eb4f75b9e3c4a600503b926e974
Donelys Familia <donelysprivate@gmail.com>
2021-01-21 21:30:51 -0500

    finally adding codingbat folder

---
+++ Java_codingChallenges/src/codingbat/firstHalfStringChallenge.java
@@ -0,0 +1,24 @@
+package codingbat;
+
+public class firstHalfStringChallenge {
+
+	/***
+	 * 
+	 * Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+
+		firstHalf("WooHoo") → "Woo"
+		firstHalf("HelloThere") → "Hello"
+		firstHalf("abcdef") → "abc"
+	 */
+	public static void main(String[] args) {
+		System.out.println(firstHalf("HelloWorld"));
+
+	}
+	
+	public static String firstHalf(String str) {
+		  return str.substring(0,str.length()/2);
+		}
+
+
+}
