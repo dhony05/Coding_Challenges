2129691e8381e5dc0f3c63c2f2d97c670d2c7c04
Donelys Familia <donelysprivate@gmail.com>
2021-02-27 20:32:37 -0500

    added simply daily challenge from codingbat.com

---
+++ Java_codingChallenges/src/codingbat/Array1/makePiChallenge.java
@@ -0,0 +1,20 @@
+package codingbat.Array1;
+
+public class makePiChallenge {
+
+	
+	/***
+	 * link:https://codingbat.com/prob/p167011
+	 * Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+makePi() → [3, 1, 4]
+	 * @return
+	 */
+	public int[] makePi() {
+		  int[] pi = {3,1,4};
+		  return pi;
+		}
+
+
+}
