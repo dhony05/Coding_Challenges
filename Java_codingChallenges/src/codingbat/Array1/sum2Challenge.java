5287bb2ee9ba6fb54bf82466f1d7314f89194f5b
Donelys Familia <donelysprivate@gmail.com>
2021-03-05 23:16:57 -0500

    added link to challenge , testing 2 factor auth

--- Java_codingChallenges/src/codingbat/Array1/sum2Challenge.java
+++ Java_codingChallenges/src/codingbat/Array1/sum2Challenge.java
@@ -4,6 +4,7 @@ public class sum2Challenge {
 
 	public int sum2(int[] nums) {
 		/***
+		 * link:https://codingbat.com/prob/p190968
 		 * 
 Given an array of ints, return the sum of the first 2 elements in the array. 
 If the array length is less than 2, just sum up the elements that exist, 
