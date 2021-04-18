77f24abb10be8a6e0f4f6b4d4e81c2458fd728d5
Donelys Familia <donelysprivate@gmail.com>
2021-04-17 20:54:03 -0400

    added new challenge

---
+++ Java_codingChallenges/src/codingbat/WarmUp/MixStartChallenge.java
@@ -0,0 +1,22 @@
+package codingbat.WarmUp;
+
+public class MixStartChallenge {
+
+/**
+ * 
+ * url:https://codingbat.com/prob/p151713
+ * 
+Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+
+mixStart("mix snacks") → true
+mixStart("pix snacks") → true
+mixStart("piz snacks") → false	
+ * @param str
+ * @return
+ */
+	public boolean mixStart(String str) {
+		  return str.contains("ix");
+		}
+
+}
