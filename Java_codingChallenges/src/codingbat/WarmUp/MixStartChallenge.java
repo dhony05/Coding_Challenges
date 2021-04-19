472906f8103a99ba7d622b5eac52fd0afa300b53
Donelys Familia <donelysprivate@gmail.com>
2021-04-18 23:16:11 -0400

    update

--- Java_codingChallenges/src/codingbat/WarmUp/MixStartChallenge.java
+++ Java_codingChallenges/src/codingbat/WarmUp/MixStartChallenge.java
@@ -8,10 +8,10 @@ public class MixStartChallenge {
  * 
 Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
 
-
 mixStart("mix snacks") → true
 mixStart("pix snacks") → true
 mixStart("piz snacks") → false	
+
  * @param str
  * @return
  */
