1448891d51b226ccb1d1b090191fd67bafb9d526
Donelys Familia <donelysprivate@gmail.com>
2021-05-05 22:06:40 -0400

    added new challenge:

---
+++ Java_codingChallenges/src/codingbat/WarmUp/LastDigitChallenge.java
@@ -0,0 +1,24 @@
+package codingbat.WarmUp;
+
+public class LastDigitChallenge {
+
+	/**
+	 * 
+	 *url:https://codingbat.com/prob/p125339 
+	 * 
+Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+
+
+lastDigit(7, 17) → true
+lastDigit(6, 17) → false
+lastDigit(3, 113) → true
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public boolean lastDigit(int a, int b) {
+		  return (a%10 == b%10);
+		}
+
+}
